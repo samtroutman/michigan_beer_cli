bc077780fec6e720263a548e48eaf01fc76bc25f
Sam <sam@Sams-MacBook-Air.local>
2021-01-10 20:47:19 -0500

    updated cli for expanded list (1-25)

--- lib/michigan_beer/cli.rb
+++ lib/michigan_beer/cli.rb
@@ -18,7 +18,7 @@ class MichiganBeer::CLI
         while input != "exit"
         puts "Enter the number of the beer you'd like more information on, type list to see the beers again, or exit to exit:"
         input = gets.strip.downcase 
-        if input.to_i.between?(1,20)
+        if input.to_i.between?(1,25)
             puts beer_details(input)
         elsif input == "list"
             list_beers   
